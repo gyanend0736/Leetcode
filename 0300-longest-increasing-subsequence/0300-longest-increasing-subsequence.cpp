e3593174259e3ccab460d495257a1e5de260eb33
Gyanendra singh <142925518+gyanend0736@users.noreply.github.com>
2026-07-29 23:12:29 +0530

    Time: 3 ms (79.09%), Space: 13.9 MB (99.56%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> tail;
-        for(auto it:nums){
-            int i=0;
-            int j= tail.size();
-            while(i<j){
-                int mid=(i+j)/2;
-                if(tail[mid]>=it){
-                    j= mid;
-                }
-                else{
-                    i= mid+1;
-                }
-            }
-            
-            if(i>=tail.size()){
-                tail.push_back(it);
+       vector<int> tail;
+       for(int it:nums){
+        int i=0;
+        int j=tail.size();
+        while(i<j){
+            int mid= (i+j)/2;
+            if(tail[mid]>=it){
+                j= mid;
             }
             else{
-                tail[i]=it;
+                i= mid+1;
             }
+
         }
+        if(i<tail.size()){
+            tail[i]=it;
+        }
+        else{
+            tail.push_back(it);
+        }
+       }
 
         return tail.size();
     }
